3fe4adc67e99a4a8e455fc75f54170c9efe9789f
Icarus9913 <icaruswu66@qq.com>
2022-02-23 19:29:01 +0800

    output to shell file

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"encoding/json"
 	"errors"
@@ -23,16 +24,16 @@ import (
 var log = logging.Logger("main")
 
 var ADD_KEY = "dce.daocloud.io/parcel.ovs.network.status"
-var OVS_POOL_ETCD_PATH = "/parcel/ovs_service/pool"
+var OVS_POOL_ETCD_PATH = "/parcel/ovs_service/pool/"
 var ETCD_V6V4MAP_KEY = "/parcel/ovs_service/v6v4map"
 var PARCEL_NET_TYPE = "dce.daocloud.io/parcel.net.type"
 var PARCEL_NET_VALUE = "dce.daocloud.io/parcel.net.value"
 
-var runningMode bool
+var shellFile = "anno.sh"
+var buf *bufio.Writer
 
 func init() {
 	_ = logging.SetLogLevel("*", "INFO")
-	util.Initialize()
 }
 
 func main() {
@@ -59,6 +60,10 @@ func startCMD() {
 				Usage: "pass the specific podName that you need to patch the annotation",
 				Value: "",
 			},
+			&cli.BoolFlag{
+				Name:  "out-to-shell",
+				Usage: "use this flag to output into a shell file named: anno.sh",
+			},
 		},
 		Commands: []*cli.Command{
 			runCmd,
@@ -76,10 +81,11 @@ var runCmd = &cli.Command{
 	Action: func(cctx *cli.Context) error {
 		ctx := context.TODO()
 
-		runningMode = cctx.Bool("really-do-it")
+		runningMode := cctx.Bool("really-do-it")
 		namespace := cctx.String("namespace")
 		podName := cctx.String("podName")
-		err := run(ctx, namespace, podName)
+		out2shell := cctx.Bool("out-to-shell")
+		err := run(ctx, namespace, podName, runningMode, out2shell)
 		if nil != err {
 			return err
 		}
@@ -87,82 +93,42 @@ var runCmd = &cli.Command{
 	},
 }
 
-func run(ctx context.Context, namespace, podName string) error {
+func run(ctx context.Context, namespace, podName string, runningMode, out2shell bool) error {
+	util.Initialize()
+
 	clientset, err := util.NewK8s()
 	if nil != err {
 		return errors.New("connect to k8s failed with err: " + err.Error())
 	}
-	hostname, err := os.Hostname()
-	if nil != err {
-		return err
-	}
-	etcdurl := ""
 
-	// 获取nodeName
-	nodeList, err := clientset.CoreV1().Nodes().List(ctx, metav1.ListOptions{})
-	if nil != err {
-		log.Error(err)
-		return err
-	}
-	for _, n := range nodeList.Items {
-		if n.Name == hostname {
-			for _, v := range n.Status.Addresses {
-				if v.Type == v1.NodeInternalIP {
-					etcdurl = v.Address
-				}
-			}
-		}
-	}
-
-	endpoints, err := clientset.CoreV1().Endpoints("kube-system").Get(ctx, "dce-etcd", metav1.GetOptions{})
-	if nil != err {
-		log.Error(err)
-		return err
-	}
-	for _, sub := range endpoints.Subsets {
-		for _, v := range sub.Ports {
-			if v.Name == "client-https" {
-				etcdurl = fmt.Sprint(etcdurl, ":", v.Port)
-			}
-		}
-	}
-
-	util.ETCD_URL = etcdurl
-
-	/*	list, err := clientset.DiscoveryV1beta1().EndpointSlices("kube-system").List(ctx, metav1.ListOptions{})
+	if out2shell {
+		file, err := os.OpenFile(shellFile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0777)
 		if nil != err {
-			log.Error(err)
-			return err
+			return errors.New("Open file " + shellFile + "failed with err: " + err.Error())
 		}
-		for _, v := range list.Items {
-
-			if strings.HasPrefix(v.Name, "dce-etcd") {
-				for _, e := range v.Endpoints {
-					if *e.Hostname == nodeName {
-						etcdurl = e.Addresses[0]
-						break
-					}
-				}
-
-				for _, vv := range v.Ports {
-					if *vv.Name == "client-https" {
-						etcdurl = fmt.Sprint(etcdurl, *vv.Port)
-						break
-					}
-				}
-			}
-		}*/
+		defer file.Close()
+		buf = bufio.NewWriter(file)
+	}
 
 	// specific pod
-	if "" != podName {
-		log.Warnf("Start to get namespace %s specific pod %s annotation", namespace, podName)
+	if podName != "" {
+		log.Infof("Start to get namespace %s specific pod %s annotation", namespace, podName)
 		pod, err := clientset.CoreV1().Pods(namespace).Get(ctx, podName, metav1.GetOptions{})
 		if nil != err {
-			return err
+			return errors.New(fmt.Sprintf("Getting pod %s from k8s failed with err %v", podName, err))
 		}
+
+		netType := pod.Annotations[PARCEL_NET_TYPE]
+		if netType != "ovs" {
+			log.Warnf("Pod %s net type is %s, skip it!", podName, netType)
+			return nil
+		}
+
 		if _, ok := pod.Annotations[ADD_KEY]; !ok {
-			collect_stupid_data(ctx, clientset, *pod)
+			collect_stupid_data(ctx, clientset, *pod, runningMode, out2shell)
+			return nil
 		}
+		log.Warnf("Pod %s already has the annotation %s, skip it!", podName, ADD_KEY)
 		return nil
 	}
 
@@ -174,141 +140,164 @@ func run(ctx context.Context, namespace, podName string) error {
 
 	log.Warnf("Start to get namespace %s pods list annotation", namespace)
 	for _, pod := range podList.Items {
+		if pod.Annotations[PARCEL_NET_TYPE] != "ovs" {
+			continue
+		}
+
+		if pod.Status.Phase != v1.PodRunning {
+			continue
+		}
+
 		if _, ok := pod.Annotations[ADD_KEY]; !ok {
-			collect_stupid_data(ctx, clientset, pod)
+			collect_stupid_data(ctx, clientset, pod, runningMode, out2shell)
 		}
 	}
 	return nil
 }
 
-func collect_stupid_data(ctx context.Context, clientset *kubernetes.Clientset, pod v1.Pod) {
+func collect_stupid_data(ctx context.Context, clientset *kubernetes.Clientset, pod v1.Pod, runningMode, out2shell bool) {
 	tmp_pod_ns := pod.Namespace
 	tmp_pod_ip := pod.Status.PodIP
-	tmp_pod_net_type := pod.Annotations[PARCEL_NET_TYPE]
-	if tmp_pod_net_type == "ovs" {
-		//tmp_annotation := map[string]interface{}{}
-		var tmp_annotation model.OvsNetworkStatus
+	var tmp_annotation model.OvsNetworkStatus
 
-		// get poolName and ruleName
-		tmp_pod_net_value := pod.Annotations[PARCEL_NET_VALUE]
-		tmp_split := strings.Split(tmp_pod_net_value, ":")
+	// get poolName and ruleName
+	tmp_pod_net_value := pod.Annotations[PARCEL_NET_VALUE]
+	tmp_split := strings.Split(tmp_pod_net_value, ":")
 
-		if strings.HasPrefix(tmp_pod_net_value, "rule") {
-			//tmp_annotation["poolName"], tmp_annotation["ruleName"] = tmp_split[1], tmp_split[2]
-			tmp_annotation.PoolName, tmp_annotation.RuleName = tmp_split[1], tmp_split[2]
-		} else if strings.HasPrefix(tmp_pod_net_value, "pool") {
-			//tmp_annotation["poolName"], tmp_annotation["ruleName"] = tmp_split[1], ""
-			tmp_annotation.PoolName, tmp_annotation.RuleName = tmp_split[1], ""
-		} else {
-			log.Errorf("pod %v seems like wrong, please check it!", pod.Name)
-			return
-		}
+	if strings.HasPrefix(tmp_pod_net_value, "rule") {
+		tmp_annotation.PoolName, tmp_annotation.RuleName = tmp_split[1], tmp_split[2]
+	} else if strings.HasPrefix(tmp_pod_net_value, "pool") {
+		tmp_annotation.PoolName, tmp_annotation.RuleName = tmp_split[1], ""
+	} else {
+		log.Errorf("pod %v seems like wrong, please check it!", pod.Name)
+		return
+	}
 
-		if tmp_annotation.PoolName != "" {
-			var poolInfo model.IPPool
-			etcdV3Client, err := util.NewEtcd()
-			if nil != err {
-				log.Errorf("connect to ETCD failed with err %v", err)
-				return
-			}
-			response, err := etcdV3Client.EtcdClient.Get(ctx, OVS_POOL_ETCD_PATH, clientv3.WithPrefix())
-			if nil != err {
-				log.Errorf("get pod %v ippool information from etcd failed with err  ", pod.Name, err.Error())
-				return
-			}
+	if tmp_annotation.PoolName != "" {
+		tmp_split_poolName := strings.Split(tmp_annotation.PoolName, "-")
+		tmp_subnet_name := tmp_split_poolName[0]
+		tmp_ns := tmp_split_poolName[1]
 
-			err = json.Unmarshal(response.Kvs[0].Value, &poolInfo)
-			if nil != err {
-				log.Errorf("Unmarshal pod %v ippool %v with err %v", pod.Name, tmp_annotation.PoolName, err)
-				return
-			}
-
-			/*			tmpshit, err := json.Marshal(poolInfo)
-						if nil != err {
-							log.Error("marshal挂了, ", err)
-							return
-						}
-						log.Info("打印看看: ", string(tmpshit))*/
-
-			// vlanID
-			tmp_annotation.VlanID = strconv.Itoa(poolInfo.DisplayVlan)
-
-			if "" != poolInfo.Subnet {
-				ip_v4_network_prefix := strings.Split(poolInfo.Subnet, "/")[1]
-				// ipv4
-				tmp_annotation.Ipv4 = strings.Join([]string{tmp_pod_ip, ip_v4_network_prefix}, "/")
-			}
-
-			if "" != poolInfo.PrefixV6 {
-				ip_v6_network_prefix := strings.Split(poolInfo.PrefixV6, "/")[1]
-				tmp_ipv6 := ""
-				v6v4Map, err := v6_v4_map(ctx)
-				if nil != err {
-					log.Error("Getting v6_v4_map failed, ", err)
-					return
-				}
-
-				for k := range v6v4Map {
-					if v6v4Map[k] == tmp_pod_ip {
-						tmp_ipv6 = k
-						break
-					}
-				}
-				if "" != tmp_ipv6 {
-					// ipv6
-					tmp_annotation.Ipv6 = strings.Join([]string{tmp_ipv6, ip_v6_network_prefix}, "/")
-				}
-			}
-
-			// isDefaultRoute
-			tmp_annotation.IsDefaultRoute = "true"
-
-			// interface
-			tmp_annotation.Interface = "eth0"
-
-			// defaultRouteV4
-			tmp_annotation.DefaultRouteV4 = poolInfo.DefaultRouteV4
-
-			// defaultRouteV6
-			tmp_annotation.DefaultRouteV6 = poolInfo.DefaultRouteV6
-
-			// route
-			if len(poolInfo.Route) == 0 {
-				tmp_annotation.Route = []string{""}
-			} else {
-				tmp_annotation.Route = poolInfo.Route
-			}
-		}
-
-		marshalData, err := json.Marshal([]interface{}{tmp_annotation})
+		var poolInfo model.IPPool
+		etcdV3Client, err := util.NewEtcd()
 		if nil != err {
-			log.Errorf("Marshal pod %v ippool %v with err %v", pod.Name, tmp_annotation.PoolName, err)
+			log.Errorf("connect to ETCD failed with err %v", err)
 			return
 		}
-		if runningMode {
-			pod.Annotations[ADD_KEY] = string(marshalData)
-			var anno = map[string]string{ADD_KEY: "'" + string(marshalData) + "'"}
-			patchData, err := json.Marshal(anno)
+
+		tmp_prefix := fmt.Sprint(OVS_POOL_ETCD_PATH, tmp_subnet_name, "/")
+		if tmp_ns != "" {
+			tmp_prefix = fmt.Sprint(tmp_prefix, tmp_ns, "/")
+		}
+		tmp_prefix = fmt.Sprint(tmp_prefix, tmp_annotation.PoolName)
+		response, err := etcdV3Client.EtcdClient.Get(ctx, tmp_prefix, clientv3.WithPrefix())
+		if nil != err {
+			log.Errorf("get pod %s ippool information from etcd failed with err  %v", pod.Name, err.Error())
+			return
+		}
+		if len(response.Kvs) == 0 {
+			log.Errorf("Gtting pod %s with no ippool %s data from ETCD, please check ETCD with key: %s", pod.Name, tmp_annotation.PoolName, tmp_prefix)
+			return
+		}
+
+		err = json.Unmarshal(response.Kvs[0].Value, &poolInfo)
+		if nil != err {
+			log.Errorf("Unmarshal pod %s ippool %s with err %v", pod.Name, tmp_annotation.PoolName, err)
+			return
+		}
+
+		// vlanID
+		tmp_annotation.VlanID = strconv.Itoa(poolInfo.DisplayVlan)
+
+		if poolInfo.Subnet != "" {
+			ip_v4_network_prefix := strings.Split(poolInfo.Subnet, "/")[1]
+			// ipv4
+			tmp_annotation.Ipv4 = strings.Join([]string{tmp_pod_ip, ip_v4_network_prefix}, "/")
+		}
+
+		if poolInfo.PrefixV6 != "" {
+			ip_v6_network_prefix := strings.Split(poolInfo.PrefixV6, "/")[1]
+			tmp_ipv6 := ""
+			v6v4Map, err := v6_v4_map(ctx)
 			if nil != err {
-				log.Errorf("Marshal pod %v patchData with err %v", pod.Name, err)
+				log.Error(fmt.Sprintf("Pod %s Getting v6_v4_map failed %v ", pod.Name, err))
 				return
 			}
 
-			_, err = clientset.CoreV1().Pods(tmp_pod_ns).Patch(ctx, pod.Name, types.StrategicMergePatchType, patchData, metav1.PatchOptions{})
-			if nil != err {
-				log.Errorf("patch pod %v annotation %v with err %v", pod.Name, string(marshalData), err)
-				return
+			for k := range v6v4Map {
+				if v6v4Map[k] == tmp_pod_ip {
+					tmp_ipv6 = k
+					break
+				}
 			}
+			if tmp_ipv6 != "" {
+				// ipv6
+				tmp_annotation.Ipv6 = strings.Join([]string{tmp_ipv6, ip_v6_network_prefix}, "/")
+			}
+		}
+
+		// isDefaultRoute
+		tmp_annotation.IsDefaultRoute = "true"
+
+		// interface
+		tmp_annotation.Interface = "eth0"
+
+		// defaultRouteV4
+		tmp_annotation.DefaultRouteV4 = poolInfo.DefaultRouteV4
+
+		// defaultRouteV6
+		tmp_annotation.DefaultRouteV6 = poolInfo.DefaultRouteV6
+
+		// route
+		if len(poolInfo.Route) == 0 {
+			tmp_annotation.Route = []string{""}
 		} else {
-			anno := model.AnnotationPatch{model.Metadata{model.Annotations{string(marshalData)}}}
-			patchResult, err := json.Marshal(anno)
-			if nil != err {
-				log.Errorf("Marshal pod %v annotation %v with err %v", pod.Name, string(marshalData), err)
-				return
-			}
-			log.Infof("Please run the command by yourself: kubectl patch pod %s --patch '%s'", pod.Name, string(patchResult))
+			tmp_annotation.Route = poolInfo.Route
 		}
 	}
+
+	marshalData, err := json.Marshal([]interface{}{tmp_annotation})
+	if nil != err {
+		log.Errorf("Marshal pod %s ippool % with err %v", pod.Name, tmp_annotation.PoolName, err)
+		return
+	}
+
+	// 自动帮pod打上annotation
+	if runningMode {
+		pod.Annotations[ADD_KEY] = string(marshalData)
+		var anno = map[string]string{ADD_KEY: "'" + string(marshalData) + "'"}
+		patchData, err := json.Marshal(anno)
+		if nil != err {
+			log.Errorf("Marshal pod %s patchData with err %v", pod.Name, err)
+			return
+		}
+
+		_, err = clientset.CoreV1().Pods(tmp_pod_ns).Patch(ctx, pod.Name, types.StrategicMergePatchType, patchData, metav1.PatchOptions{})
+		if nil != err {
+			log.Errorf("patch pod %s annotation %v with err %v", pod.Name, string(marshalData), err)
+			return
+		}
+		return
+	}
+
+	anno := model.AnnotationPatch{model.Metadata{model.Annotations{string(marshalData)}}}
+	patchResult, err := json.Marshal(anno)
+	if nil != err {
+		log.Errorf("Marshal pod %s annotation %s with err %v", pod.Name, string(marshalData), err)
+		return
+	}
+
+	if out2shell {
+		tmp_data := fmt.Sprintf("kubectl patch pod %s --patch '%s' \n", pod.Name, string(patchResult))
+		_, err := buf.Write([]byte(tmp_data))
+		if nil != err {
+			log.Errorf("Write [  %s  ] to file with err %v", tmp_data, err)
+		}
+		buf.Flush()
+		return
+	}
+	log.Infof("Please run the command by yourself: kubectl patch pod %s --patch '%s'", pod.Name, string(patchResult))
+
 }
 
 // ipv6_ipv4_map
@@ -329,7 +318,7 @@ func v6_v4_map(ctx context.Context) (map[string]string, error) {
 	for _, v := range response.Kvs {
 		err := json.Unmarshal(v.Value, &v6v4map)
 		if nil != err {
-			return nil, err
+			return nil, errors.New("Unmarshal ipv6_ipv4_map failed with error: " + err.Error())
 		}
 	}
 	return v6v4map, nil
--- util/config.go
+++ util/config.go
@@ -1,7 +1,14 @@
 package util
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"os"
+	"strconv"
+
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // etcd-configuration
@@ -9,6 +16,8 @@ var ETCD_URL = "https://1.1.1.1:12379"
 var ETCD_CA = "./certs/etcd-secrets/etcd-ca"
 var ETCD_KEY = "./certs/etcd-secrets/etcd-key"
 var ETCD_CERT = "./certs/etcd-secrets/etcd-cert"
+
+var ETCD_CER_Prefix = "etcd-secrets"
 
 // kubernetes-configuration
 var KUBE_URL = "https://1.1.1.1:11081"
@@ -45,38 +54,52 @@ func initialize() {
 
 func Initialize() {
 	//configMapPath := os.Getenv("ENV_SYSTEM__CONFIGMAP_PATH")
-	prefix := "etcd-secrets"
-	if !IsDir(prefix) {
-		log.Info("不存在/etcd-secrets证书目录文件")
+
+	// 获取ETCD URL
+	etcd_host, etcd_https_port := os.Getenv("DCE_ETCD_SERVICE_HOST"), os.Getenv("DCE_ETCD_SERVICE_PORT_CLIENT_HTTPS")
+	if etcd_host != "" && etcd_https_port != "" {
+		ETCD_URL = fmt.Sprint("https://", etcd_host, ":", etcd_https_port)
+		log.Info("Get ETCD URL from env: ", ETCD_URL)
+	} else if etcdUrl, err := GetEtcdUrl(); nil == err {
+		ETCD_URL = *etcdUrl
+		log.Info("Get ETCD URL from K8s Endpoints: ", ETCD_URL)
 	} else {
-		log.Info("本地存在etcd-secrets证书目录文件")
+		log.Info("Use the default ETCD URL: ", ETCD_URL)
 	}
-	EtcdConfigCaPath = prefix + "/etcd-ca"
-	EtcdConfigCertPath = prefix + "/etcd-cert"
-	EtcdConfigKeyPath = prefix + "/etcd-key"
-	EtcdConfigEndpoint = "https://" + os.Getenv("DCE_ETCD_SERVICE_HOST") + ":" + os.Getenv("DCE_ETCD_SERVICE_PORT_CLIENT_HTTPS")
+
+	if !IsDir(ETCD_CER_Prefix) {
+		log.Info("不存在/etcd-secrets证书目录文件, 使用默认配置")
+		return
+	}
+
+	log.Infof("本地存在%s证书目录文件", ETCD_CER_Prefix)
+
+	ETCD_CA, ETCD_CERT, ETCD_KEY = "", "", ""
+	EtcdConfigCaPath = ETCD_CER_Prefix + "/etcd-ca"
+	EtcdConfigCertPath = ETCD_CER_Prefix + "/etcd-cert"
+	EtcdConfigKeyPath = ETCD_CER_Prefix + "/etcd-key"
 
 	if existFile(EtcdConfigCaPath) {
 		ETCD_CA = EtcdConfigCaPath
 	} else {
-		log.Info("EtcdConfigCaPath不存在")
+		log.Error("EtcdConfigCaPath不存在")
+		return
 	}
 
 	if existFile(EtcdConfigCertPath) {
 		ETCD_CERT = EtcdConfigCertPath
 	} else {
-		log.Info("EtcdConfigCertPath不存在")
+		log.Error("EtcdConfigCertPath不存在")
+		return
 	}
 
 	if existFile(EtcdConfigKeyPath) {
 		ETCD_KEY = EtcdConfigKeyPath
 	} else {
-		log.Infof("EtcdConfigKeyPath不存在")
+		log.Error("EtcdConfigKeyPath不存在")
+		return
 	}
 
-	if ETCD_URL == "" {
-		ETCD_URL = EtcdConfigEndpoint
-	}
 }
 
 func IsDir(path string) bool {
@@ -85,4 +108,55 @@ func IsDir(path string) bool {
 		return false
 	}
 	return s.IsDir()
+}
+
+// 从k8s endpoints里找etcd url
+func GetEtcdUrl() (*string, error) {
+	ctx := context.TODO()
+
+	clientset, err := NewK8s()
+	if nil != err {
+		return nil, errors.New("connect to k8s failed with err: " + err.Error())
+	}
+	hostname, err := os.Hostname()
+	if nil != err {
+		return nil, errors.New("get Hostname failed with err: " + err.Error())
+	}
+
+	// 获取etcd url
+	etcdurl := "https://"
+	tmpHost := ""
+	tmpPort := ""
+	nodeList, err := clientset.CoreV1().Nodes().List(ctx, metav1.ListOptions{})
+	if nil != err {
+		return nil, errors.New("get k8s nodes failed with err: " + err.Error())
+	}
+	for _, n := range nodeList.Items {
+		if n.Name == hostname {
+			for _, v := range n.Status.Addresses {
+				if v.Type == v1.NodeInternalIP {
+					tmpHost = v.Address
+					break
+				}
+			}
+		}
+	}
+
+	endpoints, err := clientset.CoreV1().Endpoints("kube-system").Get(ctx, "dce-etcd", metav1.GetOptions{})
+	if nil != err {
+		return nil, errors.New("get k8s endpoints failed with err: " + err.Error())
+	}
+	for _, sub := range endpoints.Subsets {
+		for _, v := range sub.Ports {
+			if v.Name == "client-https" {
+				tmpPort = strconv.Itoa(int(v.Port))
+				break
+			}
+		}
+	}
+	if tmpHost != "" && tmpPort != "" {
+		etcdurl = fmt.Sprint(etcdurl, tmpHost, ":", tmpPort)
+		return &etcdurl, nil
+	}
+	return nil, errors.New("get etcd url from k8s endpoint failed")
 }
